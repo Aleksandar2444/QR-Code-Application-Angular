eb2c68b0eb2540d6749c7dcdc91c903b1c5b8f89
Antonio Ardzanliev <aardzanliev@gmail.com>
2022-08-24 22:34:28 +0200

    Failed atempt for Over 12AM

--- coffeein-to-go/src/app/components/drinks-without-coffein/drinks-without-coffein.component.ts
+++ coffeein-to-go/src/app/components/drinks-without-coffein/drinks-without-coffein.component.ts
@@ -12,6 +12,8 @@ export class DrinksWithoutCoffeinComponent implements OnInit {
 
   public today:Date = new Date();
 
+
+
   public drinksWithoutCoffein: Entity[] = [
     {
       name: 'Чај',
@@ -30,22 +32,152 @@ export class DrinksWithoutCoffeinComponent implements OnInit {
 
   ngOnInit(): void {
 
-    let ninePm = new Date();
-    ninePm.setHours(21,0,0);
-    let fiveAm = new Date();
-    fiveAm.setDate(fiveAm.getDate() + 1);
-    fiveAm.setHours(5,0,0);
 
 
-    if(this.today.getDay() === 5 || this.today.getDay() === 6) {
+    console.log(this.today);
 
-      if(this.today >= ninePm && this.today < fiveAm) {
+    const today = new Date();
+    const first = today.getDate() - today.getDay() + 1;
+
+    const fifth = first + 2;
+    const friday = new Date(today.setDate(fifth));
+
+
+
+    const sixth = first + 3;
+    const saturday = new Date(today.setDate(sixth));
+
+
+
+  console.log(friday);
+  console.log(saturday);
+
+
+    // let testDate = new Date();
+    // testDate.setHours(21,0,0);
+
+    // let testDate2 = new Date();
+    // testDate2.setDate(testDate.getDate() + 1)
+    // testDate2.setHours(5,0,0);
+
+    if(this.today.getDate()=== friday.getDate() || this.today.getDate() === saturday.getDate() ) {
+
+      friday.setHours(21,0,0);
+
+      if(this.today.getDate() === saturday.getDate()) {
+
+
+
+        saturday.setDate(saturday.getDate() + 1);
+      }
+
+      saturday.setHours(5,0,0);
+
+      if(this.today > friday && this.today < saturday) {
+
         this.cocktailBool = true;
       }
       else {
         this.cocktailBool = false;
       }
     }
+
+
+
+
+
+    console.log(this.today);
+    console.log(this.today.getDay());
+
+
+    // if(this.today.getDay() === 3) {
+
+    //   let fridayninePm = new Date();
+    //   fridayninePm.setHours(21,0,0);
+    //   let saturdayfiveAm = new Date();
+    //   saturdayfiveAm.setDate(saturdayfiveAm.getDate() + 1);
+    //   saturdayfiveAm.setHours(5,0,0);
+
+    //   console.log(fridayninePm);
+    //   console.log(saturdayfiveAm);
+
+    //   let friday2Am = new Date();
+    //   friday2Am.setHours(2,0,0);
+
+
+
+    //   if(this.today < friday2Am) {
+    //     this.cocktailBool = true;
+    //     return;
+
+    //   }
+
+
+    //   if(this.today >= fridayninePm && this.today < saturdayfiveAm) {
+    //     this.cocktailBool = true;
+    //   }
+    //   else {
+    //     this.cocktailBool = false;
+    //   }
+    // }
+    // else if(this.today.getDay() === 6) {
+
+    //   // let saturday12Am = new Date();
+    //   // saturday12Am.setHours(24,0,0);
+
+
+
+
+
+    //   let saturdayNinePm = new Date();
+    //   // saturdayNinePm.setDate(saturdayNinePm.getDate() + 1);
+    //   saturdayNinePm.setHours(21,0,0);
+    //   let sundayFiveAm = new Date();
+
+    //   sundayFiveAm.setDate(sundayFiveAm.getDate() + 1);
+    //   sundayFiveAm.setHours(5,0,0);
+
+    //   let saturday2Am = new Date();
+    //   saturday2Am.setHours(2,0,0);
+
+
+    //   if(this.today < saturday2Am) {
+    //     this.cocktailBool = true;
+    //     return;
+
+    //   }
+
+
+
+    //   if(this.today >= saturdayNinePm && this.today < sundayFiveAm) {
+
+    //     this.cocktailBool = true;
+
+    //   }
+    //   else {
+
+    //     this.cocktailBool = false;
+    //   }
+
+    // }
+    // else if(this.today.getDay() === 0) {
+
+    //   let sundayFiveAm = new Date();
+    //   sundayFiveAm.setHours(5,0,0);
+
+    //   console.log(sundayFiveAm);
+
+
+    //   if(this.today < sundayFiveAm) {
+
+    //     this.cocktailBool = true;
+    //   }
+    //   else {
+
+    //     this.cocktailBool = false;
+    //   }
+
+    // }
   }
 
 }
