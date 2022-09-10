1a2e5916fe66e079120d85e0502f7eced519eb1b
Antonio Ardzanliev <aardzanliev@gmail.com>
2022-09-10 13:11:08 +0200

    Color Toggle Test

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 // import {fadeInOnEnterAnimation, fadeOutOnLeaveAnimation} from 'angular-animations'
 
@@ -9,9 +9,26 @@ import { Router } from '@angular/router';
   // animations: [fadeInOnEnterAnimation(), fadeOutOnLeaveAnimation()]
 })
 export class HomeComponent implements OnInit {
+  test: boolean = false;
+ element = document.body;
+
   public year = new Date().getFullYear();
 
   constructor(public router: Router) {}
 
-  ngOnInit(): void {}
+
+  ngOnInit(): void {
+    this.test = true;
+
+
+  }
+
+  ToggleColor() {
+    this.element.classList.toggle("backgroundTogglerIn");
+  }
+
+  // toggleColorOut() {
+  //   let element = document.body;
+  //   element.classList.toggle("backgroundTogglerOut")
+  // }
 }
